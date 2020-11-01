6914b295c7483bf18818bef38d12e109420f425f
Nannos2013 <71235343+Nannos2013@users.noreply.github.com>
2020-11-01 04:25:17 -0600

    Update while.sh

--- while.sh
+++ while.sh
@@ -1,13 +1,7 @@
-#!/usr/bin/bash                          // Demosntrate while lopp in linux, the code within the do and done will keep getting executed if the condition is true, if it's false it stops
+#!/usr/bin/bash                          
 
-counter=2                               // counter variable is equal to 2
-while [ $counter -lt 3 ]; do            // condition is within brackets, when the counter variable is less than 3 then:
-	let counter+=1                 // counter will increase by 1 till it's equal to 3
-	echo $counter                  // print all the counter instances
-done                                   // end
-
-
-
-			// in this case, since the condition is true, the only thing that will be printed out is: 3.
-			// let's say that counter was equal to 1 on line 3. What we would have printed then is: 2 3.
-			// if counter was 4 then the condition would be false and the numbers won't print
+counter=2                               
+while [ $counter -lt 3 ]; do            
+	let counter+=1                 
+	echo $counter                  
+done                    
