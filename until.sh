a0ad3603ee78beb4aade70b87b7ae521b2898e08
Nannos2013 <71235343+Nannos2013@users.noreply.github.com>
2020-11-01 04:40:33 -0600

    Update until.sh

--- until.sh
+++ until.sh
@@ -1,7 +1,9 @@
-#!/usr/bin/bash
+#!/usr/bin/bash                                  // in the until loop the code between do and done is reepatedly excecuted until the condition changes from false to true
 
-counter=6
-until [ $counter -lt 3 ]; do
-	let counter-=1
-	echo $counter
+counter=6                                        // counter equals 6
+until [ $counter -lt 3 ]; do                     // keep executing till counter is equals to 3 and then stop
+	let counter-=1                           // the formula is counter-1
+	echo $counter                            // execute
 done
+
+                                                  // when counter equals to 3(-1) then the code stops, so we would have this printed: 5, 4, 3, 2.
