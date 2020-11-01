339298166bbe4d63149c5fb3557b96381bc487b9
Nannos2013 <71235343+Nannos2013@users.noreply.github.com>
2020-11-01 04:06:12 -0600

    Update if_else.sh

--- if_else.sh
+++ if_else.sh
@@ -1,10 +1,10 @@
-#!/usr/bin/bash                                          // Demonstrate how to code an if/then/else construct in linux
+#!/usr/bin/bash                                          
 
-num_a=400                                               // number a which is 400 in this case
-num_b=200                                              //  number b which is 200 in this case	 
+num_a=400                                          
+num_b=200                                              
 
-if [ $num_a -lt $num_b ]; then                        // The operation inside bracket is showing the condition where a is less than
-	echo "$num_a ls less than $num_b!"           // print out a is less than b
-else                                                 // if that's not true
-	echo "$num_a is greather than $num_b!"       // print out a is greater than b
-fi                                                  // this is a control statement
+if [ $num_a -lt $num_b ]; then                        
+	echo "$num_a ls less than $num_b!"           
+else                                                 
+	echo "$num_a is greather than $num_b!"       
+fi                                                  
